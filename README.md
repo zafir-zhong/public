e30d7652d0ab4df2332e01819513a3b6f64a5818
zhongzixuan <1309387239@qq.com>
2021-08-01 23:16:25 +0800

    提交一个用于模拟的服务

--- README.md
+++ README.md
@@ -18,3 +18,8 @@ k8s部署逻辑为：通过挂载本地目录覆盖config内容，并通过节�
 基于renren-generator改造   
 当前实现了：swagger/lombok通过配置开启开启，优化了响应类生成，请通过application.yml和generator.properties配置  
 远期目标为：可视化选择连接数据库（或提交sql）、选择是否包含swagger、lombok、用户信息，直接生成下载  
+
+
+
+# simple-service
+提供一个模拟服务的模板，分别有个参数控制进入返回成功还是失败的文件
---
+++ simple-service/src/main/java/top/zzx1996/simapleservice/config/FormatConfig.java
@@ -0,0 +1,31 @@
+package top.zzx1996.simapleservice.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+/**
+ * 测试配置
+ *
+ * @author ：zafir zhong
+ * @version : 1.0.0
+ * @date ：Created in 2021/6/3 下午6:08
+ * @modified By：
+ */
+@Configuration
+public class FormatConfig {
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters(fastConverter);
+    }
+}
+
